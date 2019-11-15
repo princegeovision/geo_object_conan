e4753bc7800ab2faeeb70066c839e8f667fe37d5
prince@SW4 <prince.geovision.tw@gmail.com>
2019-11-15 14:45:43 +0800

    用 test package 代替 test_main

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,9 +1,28 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
 #include <iostream>
 #include "geo_object.h"
 
-int main() {
+TEST_CASE( "get library version", "[basic]" ) {
 
     char* result = geo_object_version();
-    
-    printf("result=%s\n", result); 
+    REQUIRE( std::string(result) == std::string("geo_object_version = v1.1.0") );
 }
+
+TEST_CASE( "create geo_object library", "[basic]" ) {
+
+    geo_object* obj = nullptr;
+    obj = geo_object_create();
+    REQUIRE( obj != nullptr );
+}
+
+
+//#include <iostream>
+//#include "geo_object.h"
+//
+//int main() {
+//
+//    char* result = geo_object_version();
+//
+//    printf("result=%s\n", result);
+//}
